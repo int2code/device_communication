42986c9ef27d1322c4728992568f7596f99f765d
Piotr Sznapka <piotr.sznapka@int2code.com>
2025-05-03 13:54:27 +0200

    Add base simulator, improve communication handler and add simple example.

--- src/device_communication/base/datatypes.py
+++ src/device_communication/base/datatypes.py
@@ -1,5 +1,9 @@
 """Additional/extended datatypes"""
 
+from datetime import datetime
+
+from plum.structure import Structure
+
 
 class ByteArrayWithTimestamp(bytearray):
     """Bytearray with additional timestamp filed."""
@@ -7,3 +11,23 @@ class ByteArrayWithTimestamp(bytearray):
     def __init__(self, *args, **kwargs):
         self.timestamp = kwargs.pop("timestamp", None)
         super().__init__(*args, **kwargs)
+
+
+class PlumStructure(Structure):
+    """Plum structure with additional timestamp field."""
+
+    __timestamp = None
+
+    @property
+    def __timestamp__(self) -> datetime:
+        """Metadata to store the time the message was created or received."""
+        return self.__timestamp
+
+    @__timestamp__.setter
+    def __timestamp__(self, value: datetime):
+        """Set metadata to store the time the message was created or received.
+
+        :param value: timestamp
+
+        """
+        self.__timestamp = value
